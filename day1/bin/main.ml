1800d3940769c5b901aa841293f81abc53415649
Greg Shiner <greg@gregshiner.com>
2024-12-01 18:35:36 -0500

    Day 1 Part 1

---
+++ day1/bin/main.ml
@@ -0,0 +1,17 @@
+open Core
+let filename = "input"
+
+let lines = In_channel.read_lines filename
+
+(* Lines look like "84283   63343". All numbers are always 5 digits so it can just be normally string sliced *)
+(* This splits the line into the 2 numbers and converts them to ints *)
+let process_line line = (Int.of_string (String.slice line 0 5), Int.of_string(String.slice line 8 13))
+(* Convert the list of pairs into a pair of lists *)
+let (a, b) = Stdlib.List.split (List.map lines ~f:process_line)
+let a_sorted = List.sort a ~compare:(-)
+let b_sorted = List.sort b ~compare:(-)
+(* Calculate the distance between 2 numbers *)
+let distance a b = Int.abs (a - b)
+let distances = List.map2_exn a_sorted b_sorted ~f:distance
+let ans = List.fold distances ~init:0 ~f:(+)
+let () = print_endline (Int.to_string ans)
