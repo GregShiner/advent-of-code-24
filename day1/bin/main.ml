f5cc9e4675975300cd80a66e5baf642307251536
Greg Shiner <greg@gregshiner.com>
2024-12-01 19:15:07 -0500

    Day 1 Part 2

--- day1/bin/main.ml
+++ day1/bin/main.ml
@@ -8,10 +8,16 @@ let lines = In_channel.read_lines filename
 let process_line line = (Int.of_string (String.slice line 0 5), Int.of_string(String.slice line 8 13))
 (* Convert the list of pairs into a pair of lists *)
 let (a, b) = Stdlib.List.split (List.map lines ~f:process_line)
+(* PART 1 *)
 let a_sorted = List.sort a ~compare:(-)
 let b_sorted = List.sort b ~compare:(-)
 (* Calculate the distance between 2 numbers *)
 let distance a b = Int.abs (a - b)
 let distances = List.map2_exn a_sorted b_sorted ~f:distance
 let ans = List.fold distances ~init:0 ~f:(+)
+let () = print_endline (Int.to_string ans)
+(* PART 2 *)
+let similarity value = value * (List.count b ~f:((=) value))
+let similarities = List.map a ~f:similarity
+let ans = List.fold similarities ~init:0 ~f:(+)
 let () = print_endline (Int.to_string ans)
