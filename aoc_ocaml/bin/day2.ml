c2ae93cca6510d5dda2bc913821f0781acb2f4e8
Greg Shiner <greg@gregshiner.com>
2024-12-02 19:16:24 -0500

    Day 2 Part 2

--- aoc_ocaml/bin/day2.ml
+++ aoc_ocaml/bin/day2.ml
@@ -28,7 +28,7 @@ let is_valid_report (report : level_comparison list) : bool =
             | Descending -> true
             | _ -> false))
 
-(* Part 1*)
+(* Part 1 *)
 let _ = 
     In_channel.read_lines filename
     |> List.map ~f:(String.split ~on:' ')
@@ -39,3 +39,22 @@ let _ =
     |> Int.to_string
     |> print_endline
 
+(* Part 2 *)
+let removei (lst : 'a list) (i : int) : 'a list = 
+    List.filteri lst ~f:(fun j _ -> not (Int.equal i j))
+
+let possible_removals (report : int list) : int list list =
+    List.range 0 (List.length report)
+    |> List.map ~f:(removei report)
+
+let _ = 
+    In_channel.read_lines filename
+    |> List.map ~f:(String.split ~on:' ')
+    |> List.map ~f:(List.map ~f:Int.of_string)
+    |> List.map ~f:possible_removals (* 3 dimensional list :( *)
+    |> List.map ~f:(List.map ~f:pair_list)
+    |> List.map ~f:(List.map ~f:(List.map ~f:compare_levels))
+    |> List.map ~f:(List.count ~f:is_valid_report)
+    |> List.count ~f:(fun x -> x > 0)
+    |> Int.to_string
+    |> print_endline
